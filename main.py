2ea3b519e37d8e90a3610c34211e7d788cef6584
Elielson68 <elielsonbr.com@gmail.com>
2022-01-14 21:30:46 -0300

    requirements

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from services import get_concursos_pci
+from flask import Flask, jsonify
 
-print(get_concursos_pci())
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(get_concursos_pci())
+
+app.run(host="0.0.0.0")
