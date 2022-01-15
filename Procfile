bb3774096e72b46c49b1ca653c3acd0eec328b62
Elielson68 <elielsonbr.com@gmail.com>
2022-01-14 21:52:45 -0300

    agora vai, eu acho

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn main.py -w 1
+web: gunicorn wsgi:app -w 1
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def home():
     return jsonify(get_concursos_pci())
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
+def create_app():
+    return app
